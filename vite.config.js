1ccd0bc168cbaa13fd7cb238a5ab6280263b25ac
VladPoz <vladpozdey08@gmail.com>
2026-06-14 23:56:06 +0500

    5

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/WW/',
-=======
->>>>>>> 9aa31eb (test)
 })
