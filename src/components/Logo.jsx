142151e0307da3982be5683d249433f96bd52fa8
VladPoz <vladpozdey08@gmail.com>
2026-06-14 23:42:34 +0500

    1

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ function Logo() {
             {/* <div className="logo-text-wrapper">
                 <div className="logo-text">Юридическая компания</div>
             </div> */}
-            <img className="logo-img" src="/WW/images/logo.svg" alt="logo" />
+            <img className="logo-img" src="/images/logo.svg" alt="logo" />
         </div>
     )
 }
